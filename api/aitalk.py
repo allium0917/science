766099275553bf20b4f6d015b7e3271544452850
allium0917 <sistus0917@gmail.com>
2025-09-09 16:04:43 +0900

    reset

--- api/aitalk.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from service.aitalk_service import start_talk
-from schemas.aitalk import AITalkInput
-
-router = APIRouter(prefix="/aitalk")
-
-@router.post("/")
-def gpt_chat(payload: AITalkInput):
-    return start_talk(payload)
--- schemas/aitalk.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class AITalkInput(BaseModel):
-    uid: int
-    topic: str
-    user_input: str
